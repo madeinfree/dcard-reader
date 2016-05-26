551222c5b6de4301a20595f60127d15961cc99a4
whien <sal95610@gmail.com>
2016-05-26 12:00:04 +0800

    fixed webpack.prod.config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -28,6 +28,14 @@ module.exports = {
       },
       {
         test: /\.css$/, loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&minetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg|jpe?g|png|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
       }
     ]
   },
